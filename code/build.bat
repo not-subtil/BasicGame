6f615f766442673f5e21e70b74df41e6a572b482
Pedro Cunha <pdroferreiracunha@gmail.com>
2024-07-25 12:59:33 +0100

    End-of-line normalization

--- code/build.bat
+++ code/build.bat
@@ -4,9 +4,9 @@ REM Review CommonCompilerFlags
 set CommonLinkerFlags= -incremental:no -opt:ref user32.lib
 set CommonCompilerFlags= -MTd -nologo -Gm- -GR- -EHa- -Od -Oi -WX -W4 -wd4201 -wd4100 -wd4189 -wd4505 -wd4702 -wd4244 -FC -Z7
 
-IF NOT EXIST ..\..\BasicGame_Build mkdir ..\..\BasicGame_Build
-pushd ..\..\BasicGame_Build
+IF NOT EXIST ..\..\basic_game_build mkdir ..\..\basic_game_build
+pushd ..\..\basic_game_build
 
 REM 32-bit build
-cl %CommonCompilerFlags% ..\BasicGame\code\main.cpp /link %CommonLinkerFlags%
+cl %CommonCompilerFlags% ..\basic_game\code\main.cpp /link %CommonLinkerFlags%
 popd
--- code/main.cpp
+++ code/main.cpp
@@ -1,5 +1,11 @@
 #include "main.h"
 
+void update(HWND window)
+{
+    HDC handle = GetDC(window);
+    ReleaseDC(window, handle);
+}
+
 int CALLBACK
 WinMain(
     HINSTANCE instance, 
@@ -9,19 +15,18 @@ WinMain(
 {
     WNDCLASSA window_class = {};
 	window_class.style = CS_HREDRAW|CS_VREDRAW;
-	window_class.lpfnWndProc = win_proc_callback;
+    window_class.lpfnWndProc = win_proc_callback;
 	window_class.hInstance = instance;
-    window_class.lpszClassName = "basic_game_window_class";
+    window_class.lpszClassName = "basic game";
     window_class.hCursor = LoadCursorA(0, IDC_ARROW);
     
     if(!RegisterClassA(&window_class))
-    {
-        return(0);
-    }
-    HWND window_instance = CreateWindowExA(
-        0, //WS_EX_TOPMOST|WS_EX_LAYERED,
+    { return(0); }
+
+    HWND window = CreateWindowExA(
+        0,
     	window_class.lpszClassName,
-    	"Basic Game",
+    	"basic game",
     	WS_OVERLAPPEDWINDOW|WS_VISIBLE,
     	CW_USEDEFAULT,
     	CW_USEDEFAULT,
@@ -31,14 +36,25 @@ WinMain(
     	0,
     	instance,
     	0);
-    // check PatBlt and DrawBitmap
-  	if(!window_instance)
+
+  	if(!window)
+  	{ return(0); }
+  	
+  	MSG message = {};
+  	bool is_running = true;
+  	while(is_running)
   	{
-        return(0);
+      	if(GetMessageA(&message, window, 0, 0))
+      	{
+            TranslateMessage(&message);
+            DispatchMessage(&message);
+      	}
+      	
   	}
   	
-    return(0);
+  	return(0);
 }
+
 
 LRESULT CALLBACK 
 win_proc_callback(
@@ -47,7 +63,7 @@ win_proc_callback(
 	WPARAM wparam,
 	LPARAM lparam)
 {
-    LRESULT Result = 0;
+    LRESULT result = 0;
 	switch(message) 
 	{
     	case WM_DESTROY:
@@ -63,20 +79,7 @@ win_proc_callback(
     	case WM_KEYUP: 
 		{ break; }
     	case WM_PAINT:
-		{
-		  LPMONITORINFO monitor_info = {};
-		  monitor_info.cbSize = size_of(LPMONITORINFO);
-		  if (!GetMonitorInfoA(, monitor_info))
-		  { return(0); }
-		  
-		  LPPAINTSTRUCT paint = {};
-          paint->hdc = GetDC(window);
-          paint->fErase = 1;
-          paint->rcPaint = monitor_info.rcMonitor;
-		  
-          HDC paint_result = BeginPaint(window, paint);
-		  break; 
-		}
+		{ break; }
     	default:
 		{
 			result = DefWindowProcA(window, message, wparam, lparam); 
